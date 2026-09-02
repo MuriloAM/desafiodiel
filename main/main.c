a5644c6f6dabb4881f866b8b5130240e1e80f1ff
MuriloAM <muriloa.melo01@gmail.com>
2026-09-02 10:28:04 -0300

    feat: add code to start wifi in station mode

--- main/main.c
+++ main/main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
+#include "esp_event.h"
 #include "esp_err.h"
+#include "esp_wifi.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 #include "nvs_flash.h"
+
+static void app_wifi_init()
+{
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_FLASH));
+    ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
 
 void app_main(void)
 {
@@ -19,5 +31,10 @@ void app_main(void)
         err = nvs_flash_init();
     }
     ESP_ERROR_CHECK(err);
+
+    // Inicializar sistema padrão de eventos.
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    app_wifi_init();
 
 }
