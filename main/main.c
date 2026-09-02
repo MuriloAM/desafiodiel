2eb5099dea93638cb5aae3fe3d7211139ed4453a
MuriloAM <muriloa.melo01@gmail.com>
2026-09-02 18:09:26 -0300

    feat: add mqtt capabilityies

--- main/main.c
+++ main/main.c
@@ -10,6 +10,7 @@
 
 #include "iot_button.h"
 #include "button_gpio.h"
+#include "mqtt_client.h"
 #include "nvs_flash.h"
 #include "sdkconfig.h"
 
@@ -21,6 +22,13 @@
 #define APP_BUTTON_SHORT_PRESS_TIME (200)
 #define APP_BUTTON_0_ACTIVE_LEVEL (0)
 #define APP_BUTTON_0_GPIO (0)
+// MQTT.
+#define MQTT_BROKER_URI "mqtts://broker.emqx.io:8883" //"mqtt.eclipse.org" //"broker.hivemq.com"//
+#define MQTT_TOPIC_CMD_SUB "desafiodiel/murilo/esp32/cmd"
+#define MQTT_TOPIC_CMD_PUB "desafiodiel/murilo/esp32/status"
+/* Embedded Mosquitto CA certificate for test.mosquitto.org:8883 */
+extern const uint8_t mqtt_crt_start[] asm("_binary_broker_emqx_io_ca_crt_start");
+extern const uint8_t mqtt_crt_end[] asm("_binary_broker_emqx_io_ca_crt_end");
 
 // statics variables.
 static const char *TAG = "esp_app";
@@ -29,8 +37,9 @@ static esp_event_handler_instance_t instance_any_id = NULL;
 static esp_event_handler_instance_t instance_got_ip = NULL;
 
 // functions prototypes.
-static void app_wifi_init(void);
 static esp_err_t app_button_init(button_handle_t *btn);
+static void app_mqtt_init(void);
+static void app_wifi_init(void);
 static void button_single_click_event_cb(void *arg, void *data);
 static void button_double_click_event_cb(void *arg, void *data);
 
@@ -54,6 +63,64 @@ static void event_handler(void *arg, esp_event_base_t event_base,
     {
         ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
         ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        app_mqtt_init();
+    }
+}
+
+static void mqtt_event_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data)
+{
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    switch ((esp_mqtt_event_id_t)event_id)
+    {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        msg_id = esp_mqtt_client_subscribe(client, MQTT_TOPIC_CMD_SUB, 0);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+        msg_id = esp_mqtt_client_publish(client, MQTT_TOPIC_CMD_PUB, "data", 0, 0, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d, return code=0x%02x ", event->msg_id, (uint8_t)*event->data);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT)
+        {
+            ESP_LOGI(TAG, "Last error code reported from esp-tls: 0x%x", event->error_handle->esp_tls_last_esp_err);
+            ESP_LOGI(TAG, "Last tls stack error number: 0x%x", event->error_handle->esp_tls_stack_err);
+            ESP_LOGI(TAG, "Last captured errno : %d (%s)", event->error_handle->esp_transport_sock_errno,
+                     strerror(event->error_handle->esp_transport_sock_errno));
+        }
+        else if (event->error_handle->error_type == MQTT_ERROR_TYPE_CONNECTION_REFUSED)
+        {
+            ESP_LOGI(TAG, "Connection refused error: 0x%x", event->error_handle->connect_return_code);
+        }
+        else
+        {
+            ESP_LOGW(TAG, "Unknown error type: 0x%x", event->error_handle->error_type);
+        }
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
     }
 }
 
@@ -82,6 +149,24 @@ static esp_err_t app_button_init(button_handle_t *btn)
     esp_err_t ret = iot_button_new_gpio_device(&btn_cfg, &btn_gpio_cfg, &new_btn);
     *btn = new_btn;
     return ret;
+}
+
+static void app_mqtt_init()
+{
+    const esp_mqtt_client_config_t mqtt_cfg = {
+        .broker = {
+            .address.uri = MQTT_BROKER_URI,
+            .verification.certificate = (const char *)mqtt_crt_start},
+    };
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    if (client == NULL)
+    {
+        ESP_LOGE(TAG, "can't create mqtt client");
+        return;
+    }
+
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
 }
 
 static void app_wifi_init()
